327fe177aed3616eeadb86d089540d5fa6a97d1b
keikimala <chantingmara@gmail.com>
2024-04-18 14:44:30 +1200

    test 2

--- app/src/main/java/com/example/dealseeker20/MainActivity.kt
+++ app/src/main/java/com/example/dealseeker20/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
-                    // test lenalei branch
+                    // test lenalei branch 2.0
                 }
             }
         }
